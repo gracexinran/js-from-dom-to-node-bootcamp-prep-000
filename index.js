1b9bf93a8448f06ab49915e7b44634a7cd293e0f
Xinran Che <gracexinran@gmail.com>
2019-04-09 01:04:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ let person2 = "Withnail"
 
 console.log( 'Hi, how are you, ' + person2 + '?' )
 console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
+
+for ( let n = 0; n < 10; n++ ) {
+  console.log( n );
+}
